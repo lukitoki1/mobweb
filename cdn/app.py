d24e2c899b08782c92e55e248a230efcd853cf05
lukitoki1 <lukitoki1@outlook.com>
2019-11-28 22:26:48 +0100

    "nowsza" wersja

---
+++ cdn/app.py
@@ -0,0 +1,100 @@
+import jwt
+from uuid import uuid4
+from flask import Flask
+from flask import request
+from flask import make_response
+from flask import send_file
+from dotenv import load_dotenv
+from os import getenv
+import json
+import os
+
+load_dotenv(verbose=True)
+
+app = Flask(__name__)
+JWT_SECRET = getenv('JWT_SECRET')
+
+
+@app.route('/list/<uid>', methods=['GET'])
+def list(uid):
+    token = request.args.get('token')
+    if len(uid) == 0:
+        return '<h1>CDN</h1> Missing uid', 404
+    if token is None:
+        return '<h1>CDN</h1> No token', 401
+    if not valid(token):
+        return '<h1>CDN</h1> Invalid token', 401
+    payload = jwt.decode(token, JWT_SECRET)
+    if payload.get('uid') != uid or payload.get('action') != 'list':
+        return '<h1>CDN</h1> Incorrect token payload', 401
+
+    if not os.path.exists("/tmp/" + uid):
+        return json.dumps([])
+
+    listOfFiles = os.listdir("/tmp/" + uid)
+    return json.dumps(listOfFiles)
+
+
+@app.route('/files', methods=['GET'])
+def downloadd():
+    uid = request.args.get('uid')
+    token = request.args.get('token')
+    filename = request.args.get('filename')
+    if len(uid) == 0:
+        return '<h1>CDN</h1> Missing uid', 404
+    if token is None:
+        return '<h1>CDN</h1> No token', 401
+    if not valid(token):
+        return '<h1>CDN</h1> Invalid token', 401
+    payload = jwt.decode(token, JWT_SECRET)
+    if payload.get('uid') != uid or payload.get('action') != 'download':
+        return '<h1>CDN</h1> Incorrect token payload', 401
+    file = '/tmp/test/' + filename
+    file = open(file, 'rb')
+    return send_file(file, attachment_filename=filename, as_attachment=True)
+
+
+@app.route('/files', methods=['POST'])
+def upload():
+    f = request.files.get('file')
+    t = request.form.get('token')
+    c = request.form.get('callback')
+    uid = request.form.get('uid')
+    if f.filename is "":
+        return redirect(f"{c}?error=No+file+provided") if c \
+            else ('<h1>CDN</h1> No file provided', 400)
+    if t is None:
+        return redirect(f"{c}?error=No+token+provided") if c \
+            else ('<h1>CDN</h1> No token provided', 401)
+    if not valid(t):
+        return redirect(f"{c}?error=Invalid+token") if c \
+            else ('<h1>CDN</h1> Invalid token', 401)
+    payload = jwt.decode(t, JWT_SECRET)
+    if payload.get('uid') != uid or payload.get('action') != 'upload':
+        return '<h1>CDN</h1> Incorrect token payload', 401
+
+    if not os.path.exists("/tmp/" + uid):
+        os.mkdir("/tmp/" + uid)
+
+    # TODO zrobic zamykanie zeby czekalo na save'a
+    content_type = "multipart/form-data"
+    f.save('/tmp/' + uid + "/" + f.filename)
+    f.close()
+
+    return redirect(f"{c}?uid={uid}&content_type={content_type}") if c \
+        else (f'<h1>CDN</h1> Uploaded {uid}', 200)
+
+
+def valid(token):
+    try:
+        jwt.decode(token, JWT_SECRET, algorithms=['HS256'])
+    except jwt.InvalidTokenError as e:
+        app.logger.error(str(e))
+        return False
+    return True
+
+
+def redirect(location):
+    response = make_response('', 303)
+    response.headers["Location"] = location
+    return response
---
+++ web/container_data/app.py
@@ -0,0 +1,137 @@
+from uuid import uuid4
+from flask import Flask
+from flask import request
+from flask import make_response
+from flask import render_template
+from dotenv import load_dotenv
+from os import getenv
+import datetime
+import redisHandler
+import sessionHandler
+import redis
+import jwt
+import requests
+import json
+
+load_dotenv(verbose=True)
+
+HTML = """<!doctype html>
+<head><meta charset="utf-8"/></head>"""
+
+app = Flask(__name__)
+CDN = getenv("CDN_HOST")
+WEB = getenv("WEB_HOST")
+SESSION_TIME = int(getenv("SESSION_TIME"))
+JWT_SESSION_TIME = int(getenv('JWT_SESSION_TIME'))
+JWT_SECRET = getenv("JWT_SECRET")
+INVALIDATE = -1
+
+redis = redis.Redis(host="redis", port="6379")
+
+redisConn = redisHandler.RedisHandler(redis)
+redisConn.initUser()
+
+session = sessionHandler.SessionHandler(redis)
+
+
+@app.route('/')
+def index():
+    session_id = request.cookies.get('session_id')
+    if session_id is None:
+        return redirect("/login")
+    return redirect("/index")
+
+
+# TODO jesli zalogowany to blokada loginu!
+
+@app.route('/login', methods=['GET'])
+def login():
+    return render_template('login.html')
+
+
+@app.route('/index')
+def welcome():
+    session_id = request.cookies.get('session_id')
+    if session_id:
+        if session.checkSession(session_id):
+            uid = session.getNicknameSession(session_id)
+            downloadToken = createDownloadToken(uid).decode('utf-8')
+            uploadToken = createUploadToken(uid).decode('utf-8')
+            listToken = createListToken(uid).decode('utf-8')
+            listOfFiles = json.loads(requests.get("http://cdn:5000/list/" + uid + "?token=" + listToken).content)
+            print(type(listOfFiles), flush=True)
+            return render_template("index.html", uid=uid, uploadToken=uploadToken, downloadToken=downloadToken,
+                                   listToken=listToken, listOfFiles=listOfFiles)
+        else:
+            response = redirect("/login")
+            response.set_cookie("session_id", "INVALIDATE", max_age=INVALIDATE)
+            return response
+    return redirect("/login")
+
+
+@app.route('/auth', methods=['POST'])
+def auth():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    if username is not "" and password is not "":
+        response = make_response('', 303)
+
+        if redisConn.checkUser(username, password) is True:
+            session_id = session.createSession(username)
+            response.set_cookie("session_id", session_id, max_age=SESSION_TIME)
+            response.headers["Location"] = "/index"
+        else:
+            response.set_cookie("session_id", "INVALIDATE", max_age=1)
+            response.headers["Location"] = "/login"
+
+        return response
+    return redirect("/login")
+
+
+@app.route('/logout')
+def logout():
+    session_id = request.cookies.get('session_id')
+    if session_id:
+        session.deleteSession(session_id)
+        response = redirect("/login")
+        response.set_cookie("session_id", "LOGGED_OUT", max_age=1)
+        return response
+    return redirect("/login")
+
+
+@app.route('/callback')
+def uploaded():
+    session_id = request.cookies.get('session_id')
+    uid = request.args.get('uid')
+    err = request.args.get('error')
+    if not session_id:
+        return redirect("/login")
+
+    if err:
+        return f"<h1>APP</h1> Upload failed: {err}", 400
+    if not uid:
+        return f"<h1>APP</h1> Upload successfull, but no uid returned", 500
+    content_type = request.args.get('content_type', 'text/plain')
+    # session[session_id] = (uid, content_type)
+    return f"<h1>APP</h1> User {session_id} uploaded {uid} ({content_type})", 200
+
+
+def createDownloadToken(uid):
+    exp = datetime.datetime.utcnow() + datetime.timedelta(seconds=JWT_SESSION_TIME)
+    return jwt.encode({"iss": "web.company.com", "exp": exp, "uid": uid, "action": "download"}, JWT_SECRET, "HS256")
+
+
+def createUploadToken(uid):
+    exp = datetime.datetime.utcnow() + datetime.timedelta(seconds=JWT_SESSION_TIME)
+    return jwt.encode({"iss": "web.company.com", "exp": exp, "uid": uid, "action": "upload"}, JWT_SECRET, "HS256")
+
+
+def createListToken(uid):
+    exp = datetime.datetime.utcnow() + datetime.timedelta(seconds=JWT_SESSION_TIME)
+    return jwt.encode({"iss": "web.company.com", "exp": exp, "uid": uid, "action": "list"}, JWT_SECRET, "HS256")
+
+
+def redirect(location):
+    response = make_response('', 303)
+    response.headers["Location"] = location
+    return response
